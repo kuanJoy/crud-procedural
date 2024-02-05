a0a3a8d17f3940ddd2cefed6dc0d213edf920fa3
Kuanysh <kuanyshmykyev@gmail.com>
2024-02-05 22:46:40 +0600

    validate fixes

--- config/functions.php
+++ config/functions.php
@@ -77,8 +77,9 @@ function validate_pass($errors)
     global $password;
     if (empty($password)) {
         $errors["pass"] = "Введите пароль";
-    } elseif ((!preg_match('/^(?=.*[0-9])(?=.*[AZ])[0-9azA-Z]{6,20}$/u', $password))) {
+    } elseif ((!preg_match('/^(?=.*[0-9])(?=.*[A-Z])[0-9azA-Z]{6,20}$/u', $password))) {
         $errors["pass"] = "Длина пароля 6 до 20 символов. Только на латинице. Используйте хотя бы 1 цифру и заглавную букву";
+        var_dump($password);
     } else {
         $password = trim($password);
     }
